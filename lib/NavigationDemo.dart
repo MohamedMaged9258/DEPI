ec86364dd4314294c1e4178b3b91fcb5147954e0
Mohamed Maged <mohamedmaged9258@gmail.com>
2025-08-19 14:06:48 +0300

    Navigation Demo

---
+++ lib/NavigationDemo.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+class NavigationDemo extends StatelessWidget {
+  const NavigationDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Navigation Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const MainNavigator(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MainNavigator extends StatefulWidget {
+  const MainNavigator({super.key});
+
+  @override
+  State<MainNavigator> createState() => _MainNavigatorState();
+}
+
+class _MainNavigatorState extends State<MainNavigator> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    NavigationScreen(icon: Icons.home, text: 'Home Screen', color: Colors.teal),
+    NavigationScreen(
+      icon: Icons.search,
+      text: 'Search Screen',
+      color: Colors.orange,
+    ),
+    NavigationScreen(
+      icon: Icons.person,
+      text: 'Profile Screen',
+      color: Colors.blue,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Navigation Demo')),
+      body: Center(child: _widgetOptions.elementAt(_selectedIndex)),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.teal[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class NavigationScreen extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  final Color color;
+
+  const NavigationScreen({
+    super.key,
+    required this.icon,
+    required this.text,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(icon, size: 100, color: color),
+          const SizedBox(height: 20),
+          Text(
+            text,
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: color,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
